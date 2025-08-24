9ee1fbbd5b5b669187d754b3a664071589806d6a
Matias Correa <matiasjosecorrea@gmail.com>
2025-08-24 03:30:55 -0700

    Recreation.gov integration skeleton

---
+++ server/src/services/recreationGov.js
@@ -0,0 +1,87 @@
+const axios = require('axios');
+
+const BASE_URL = 'https://ridb.recreation.gov/api/v1';
+const API_KEY = process.env.RECREATION_GOV_API_KEY; // Set your API key in environment variables
+
+const defaultHeaders = {
+  apikey: API_KEY,
+};
+
+// Get all facilities
+async function getFacilities(params = {}) {
+  const response = await axios.get(`${BASE_URL}/facilities`, {
+    params,
+    headers: defaultHeaders,
+  });
+  return response.data.RECDATA || [];
+}
+
+// Get a specific facility
+async function getFacility(id) {
+  const response = await axios.get(`${BASE_URL}/facilities/${id}`, {
+    headers: defaultHeaders,
+  });
+  return response.data;
+}
+
+// Get all campsites
+async function getCampsites(params = {}) {
+  const response = await axios.get(`${BASE_URL}/campsites`, {
+    params,
+    headers: defaultHeaders,
+  });
+  return response.data.RECDATA || [];
+}
+
+// Get a specific campsite
+async function getCampsite(id) {
+  const response = await axios.get(`${BASE_URL}/campsites/${id}`, {
+    headers: defaultHeaders,
+  });
+  return response.data;
+}
+
+// Get all organizations
+async function getOrganizations(params = {}) {
+  const response = await axios.get(`${BASE_URL}/organizations`, {
+    params,
+    headers: defaultHeaders,
+  });
+  return response.data.RECDATA || [];
+}
+
+// Get a specific organization
+async function getOrganization(id) {
+  const response = await axios.get(`${BASE_URL}/organizations/${id}`, {
+    headers: defaultHeaders,
+  });
+  return response.data;
+}
+
+// Get all events
+async function getEvents(params = {}) {
+  const response = await axios.get(`${BASE_URL}/events`, {
+    params,
+    headers: defaultHeaders,
+  });
+  return response.data.RECDATA || [];
+}
+
+// Get a specific event
+async function getEvent(id) {
+  const response = await axios.get(`${BASE_URL}/events/${id}`, {
+    headers: defaultHeaders,
+  });
+  return response.data;
+}
+
+module.exports = {
+  getFacilities,
+  getFacility,
+  getCampsites,
+  getCampsite,
+  getOrganizations,
+  getOrganization,
+  getEvents,
+  getEvent,
+};
