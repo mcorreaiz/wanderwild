09eaa7835c9b7019635bef616b89d90b289f58e9
Matias Correa <matiasjosecorrea@gmail.com>
2025-08-27 00:34:10 -0700

    Display Rec Areas instead of campsites in map

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
-const GET_CAMPSITES = gql`
-  query GetCampsites {
-    campsites {
-      campsite_id
-      campsite_name
-      campsite_latitude
-      campsite_longitude
+const GET_RECREATION_AREAS = gql`
+  query GetRecreationAreas {
+    recreationAreas {
+      recreation_area_id
+      recreation_area_name
+      recreation_area_latitude
+      recreation_area_longitude
     }
   }
 `;
 
 const MapScreen: React.FC = () => {
-  // const { data, loading, error } = useQuery(GET_CAMPSITES);
+  // const { data, loading, error } = useQuery(GET_RECREATION_AREAS);
 
   // if (loading) return <View style={styles.container}><Text>Loading...</Text></View>;
   // if (error) return <View style={styles.container}><Text>Error loading campsites</Text></View>;
@@ -31,14 +31,14 @@ const MapScreen: React.FC = () => {
           longitudeDelta: 10,
         }}
       >
-        {/* {data.campsites.map((site: any) => (
+        {/* {data.recreationAreas.map((area: any) => (
           <Marker
-            key={site.campsite_id}
+            key={area.recreation_area_id}
             coordinate={{
-              latitude: site.campsite_latitude,
-              longitude: site.campsite_longitude,
+              latitude: area.recreation_area_latitude,
+              longitude: area.recreation_area_longitude,
             }}
-            title={site.campsite_name}
+            title={area.recreation_area_name}
           />
         ))} */}
       </MapView>
